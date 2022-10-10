0d326df95380f5036d0e59e9810430607386110c
seokjin <70871997+tjrwls@users.noreply.github.com>
2022-10-10 16:03:18 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.swift
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.swift
@@ -1,9 +1,3 @@
 func solution(_ phone_number:String) -> String {
-    var answer = Array(phone_number)
-    if phone_number.count != 4{
-        for index in 0...(phone_number.count-5) {
-            answer[index] = "*"
-        }
-    }
-    return String(answer)
+    return "\(String(repeating: "*", count: phone_number.count - 4))\(phone_number.suffix(4))"
 }
