9bbb2f1c288fb250c6e30aaa51dabd7ae2edc2f0
seokjin <70871997+tjrwls@users.noreply.github.com>
2022-10-22 16:55:28 +0900

    [level 1] Title: 최소직사각형, Time: 7.21 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.swift
@@ -3,16 +3,9 @@ import Foundation
 func solution(_ sizes:[[Int]]) -> Int {
     var firstNum: Int = 0
     var secondNum: Int = 0
-    
     for element in sizes{
-        if element[0] > element[1]{
-            firstNum = firstNum > element[0] ? firstNum : element[0]
-            secondNum = secondNum > element[1] ? secondNum : element[1]
-        } else {
-            firstNum = firstNum > element[1] ? firstNum : element[1]
-            secondNum = secondNum > element[0] ? secondNum : element[0]
-        }
+        firstNum = max(firstNum, element.max()!)
+        secondNum = max(secondNum, element.min()!)
     }
     return firstNum * secondNum
-    
 }
