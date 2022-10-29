7b86763d37f3e460d91aa03fef71a5f290e8e66d
seokjin <70871997+tjrwls@users.noreply.github.com>
2022-10-29 13:32:09 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.97 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -1,4 +1,4 @@
 func solution(_ n:Int, _ m:Int) -> [Int] {
-    var minNum = Array(1...min(n, m)).filter{ n % $0 == 0 && m % $0 == 0}
-    return [minNum.last!, n * m / minNum.last!]
+    var maxNum = Array(1...min(n, m)).filter{ n % $0 == 0 && m % $0 == 0}
+    return [maxNum.last!, n * m / maxNum.last!]
 }
