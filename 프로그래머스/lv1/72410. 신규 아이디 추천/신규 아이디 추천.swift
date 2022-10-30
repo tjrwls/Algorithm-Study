90986fcc03264fe6750e7743b378dfa883357e97
seokjin <70871997+tjrwls@users.noreply.github.com>
2022-10-30 17:41:04 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.38 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+func solution(_ new_id:String) -> String {
+    var answer: String = ""
+    var dotCheck: Bool = true
+    for charOfId in new_id.lowercased() {
+        switch charOfId {
+        case "0"..."9", "a"..."z", "-", "_":
+            answer += "\(charOfId)"
+            dotCheck = true
+        case ".":
+            if dotCheck {
+                answer += "\(charOfId)"
+                dotCheck = false
+            }
+        default:
+            break
+        }
+    }
+    if answer == "" || answer == "."{
+     answer = "aaa"
+    } else{
+        if answer.first! == "." {
+            answer.removeFirst()
+        }
+        if answer.last! == "." {
+            answer.removeLast()
+        }
+        switch answer.count {
+        case 1: answer += answer + answer
+        case 2: answer += "\(answer.last!)"
+        case 3...15: break
+        default:
+            answer = String(answer[answer.startIndex...answer.index(answer.startIndex, offsetBy: 14)])
+            if answer.last! == "." {
+                answer.removeLast()
+            }
+        }
+    }
+    return answer
+}
