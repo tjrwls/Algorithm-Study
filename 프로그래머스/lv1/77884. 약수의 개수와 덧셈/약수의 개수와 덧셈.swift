bb717d575226c7038ce519e69e9501b14446799d
seokjin <70871997+tjrwls@users.noreply.github.com>
2022-11-15 21:30:21 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 5.17 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
@@ -1,16 +1,5 @@
 import Foundation
 
 func solution(_ left:Int, _ right:Int) -> Int {
-    var answer: Int = 0
-    var count: Int = 0
-    for num in left...right{
-        count = 0
-        for i in 1...num {
-            if num % i == 0 {
-                count += 1
-            }
-        }
-        answer = count % 2 == 0 ? answer + num : answer - num
-    }
-    return answer
+    return ((left...right).map{ i in (1...i).filter({ i % $0 == 0}).count % 2 == 0 ? i : -i}.reduce(0, +))
 }
