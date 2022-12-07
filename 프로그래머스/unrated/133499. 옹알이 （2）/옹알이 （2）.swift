82bd958a3e8faa6f72b4ccaf8bcfcb4e7535dbc4
seokjin <70871997+tjrwls@users.noreply.github.com>
2022-12-07 18:44:32 +0900

    [unrated] Title: 옹알이 (2), Time: 0.88 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ babbling:[String]) -> Int {
+    var count: Int = 0
+    for element in babbling {
+        var str = String(element)
+        str = str.replacingOccurrences(of: "aya", with: "1")
+        str = str.replacingOccurrences(of: "ye", with: "2")
+        str = str.replacingOccurrences(of: "woo", with: "3")
+        str = str.replacingOccurrences(of: "ma", with: "4")
+        if Int(str) != nil && !str.contains("11") && !str.contains("22") && !str.contains("33") && !str.contains("44"){
+            count += 1
+        }
+    }    
+    return count
+}
