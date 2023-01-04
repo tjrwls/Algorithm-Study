00522c46c741ecfc361cdb6a803cb7809ebe11a2
seokjin <70871997+tjrwls@users.noreply.github.com>
2023-01-05 00:35:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.62 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.swift
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.swift
@@ -5,7 +5,7 @@ func solution(_ n:Int, _ words:[String]) -> [Int] {
     for (index, element) in words.enumerated(){
         if index == 0 {
             checkArr.append(element)
-        } else if (checkArr.contains(element) || element.first != checkArr[index - 1].last) || element.count == 1 {
+        } else if (checkArr.contains(element) || element.first != checkArr[index - 1].last) {
             return [ index % n + 1, index / n + 1]
         } else {
         checkArr.append(element)
