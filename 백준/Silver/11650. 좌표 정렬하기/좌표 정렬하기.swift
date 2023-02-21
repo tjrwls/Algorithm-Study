5353e1ae2c01f50e33055bcf217a06e77720d2b2
seokjin <70871997+tjrwls@users.noreply.github.com>
2023-02-21 14:28:58 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 248 ms, Memory: 87228 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let num = Int(readLine()!)!
+var answer: [[Int]] = []
+for _ in 1...num {
+    answer.append(readLine()!.split(separator: " ").map{Int($0)!})
+}
+answer.sort {($0[0], $0[1]) < ($1[0], $1[1])}
+for index in 0...(num - 1) {
+    print("\(answer[index][0]) \(answer[index][1])")
+}
