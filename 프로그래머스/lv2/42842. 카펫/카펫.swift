a17b31b0c498bc4d843a56e409a8bd7e099db2df
seokjin <70871997+tjrwls@users.noreply.github.com>
2023-05-03 13:19:08 +0900

    [level 2] Title: 카펫, Time: 0.17 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.swift
+++ 프로그래머스/lv2/42842. 카펫/카펫.swift
@@ -1,14 +1,13 @@
 import Foundation
 
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
-    var yellowArr: [Int] = Array(1...yellow).filter{ yellow % $0 == 0 }
+    let yellowArr: [Int] = Array(1...yellow).filter{ yellow % $0 == 0 }
     for yellowNum in yellowArr {
         let a = max(yellowNum, yellow / yellowNum)
         let b = min(yellowNum, yellow / yellowNum)
            if brown == a * 2 + b * 2 + 4 {
                return [a + 2, b + 2]
            }
-        print("\(a), \(b)")
     }
     return [0, 0]
 }
