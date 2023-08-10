be8025bd17020eb6270314cc35ed471361720814
seokjin <70871997+tjrwls@users.noreply.github.com>
2023-08-10 19:40:02 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.swift
+++ 프로그래머스/lv2/42578. 의상/의상.swift
@@ -1,5 +1,14 @@
 import Foundation
 
 func solution(_ clothes:[[String]]) -> Int {
-    return 0
+    var clothesDic: [String : Int] = [:]
+    var result = 0
+    func combination(_ count: [Int]) -> Int {
+        var count = count.map{$0 + 1}
+        return result + count.reduce(1, *) - 1
+    }
+    for clothes in clothes {
+        clothesDic[clothes.last!, default: 0] += 1
+    }
+    return clothesDic.count == 1 ? clothes.count : combination(clothesDic.map{$0.value})
 }
